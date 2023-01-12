81c948d0d75c24232f8564404411495eb8d074f6
Ordaz Romero Sergio <orcheko@gmail.com>
2023-01-12 01:10:39 -0600

    Preparando plantilla para funcionamiento

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
+    private var isFinishedTypeingNumber = true
+    private var currentValue: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func calcButtonpressed(_ sender: Any) {
+        isFinishedTypeingNumber = false
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let textButton = sender.currentTitle {
+            if isFinishedTypeingNumber {
+                displayLabel.text = textButton
+                isFinishedTypeingNumber = false
+            } else {
+                displayLabel.text! += textButton
+            }
+        }
+    }
+    
 }
 
